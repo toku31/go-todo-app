f9b8147ab0262e0b92288ba8898c05764357d36c
garden627 <garden627@yahoo.co.jp>
2021-07-11 00:01:19 +0900

    first commit136

---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"todo_app/app/controllers"
+	"todo_app/app/models"
+)
+
+func main() {
+
+		fmt.Println(models.Db)
+
+		controllers.StartMainServer()
+
+
+
+
+
+
+
+
+	/*
+		fmt.Println(config.Config.Port)
+		fmt.Println(config.Config.SQLDriver)
+		fmt.Println(config.Config.DbName)
+		fmt.Println(config.Config.LogFile)
+
+		log.Println("test")
+	*/
+
+	// DB作成
+	//fmt.Println(models.Db)
+
+	// user　作成
+	// u := &models.User{}
+	// u.Name = "test2"
+	// u.Email = "test2@example.com"
+	// u.PassWord = "testtest"
+	// fmt.Println(u)
+
+	// u.CreateUser()
+
+	//===============
+	// u, _ := models.GetUser(1)
+	// fmt.Println(u)
+
+	// u.Name = "Test2"
+	// u.Email = "test2@example.com"
+	// u.UpdateUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
+
+	// u.DeleteUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
+
+	// ==============
+	// user, _ := models.GetUser(2)
+	// u, _ := models.GetUser(2)
+	// u.CreateTodo("First Todo")
+
+	//===============
+	// todoの所得
+	// t, _ := models.GetTodo(1)
+	// fmt.Println(t)
+
+	// todosの取得
+	// user, _ := models.GetUser(3)
+	// user.CreateTodo("Third Todo")
+
+	// todos, _ := models.GetTodos()
+	// for _, v := range todos {
+	// 	fmt.Println(v)
+	// }
+
+	// user2, _ := models.GetUser(3)
+	// todos, _ := user2.GetTodosByUser()
+	// for _, v := range todos {
+	// 	fmt.Println(v)
+	// }
+
+	// Update
+	// t, _ := models.GetTodo(8)
+	// t.Content = "Update Todo"
+	// t.UpdateTodo()
+
+	// Delete
+	// t, _ := models.GetTodo(10)
+	// t.DeleteTodo()
+
+}
